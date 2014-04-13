5c6bf1c48feac6be0ce5212dcde28417c748c5cd
gagan-bansal <gaganbbansal123@gmail.com>
2014-04-13 05:55:22 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Converts mutli parts geojson to many single part geojsons and vice-versa.
+
+License
+This projected is licensed under the terms of the MIT license.
---
+++ index.js
@@ -0,0 +1,49 @@
+//index.js 
+(function() { 
+	var singles = ['Point', 'LineString', 'Polygon'];
+	var multies = ['MultiPoint', 'MultiLineString', 'MultiPolygon'];
+	function explode(g) {
+	  if( multies.indexOf(g.type) > -1) {
+	    return g.coordinates.map(function(part) {
+	      var single = {};
+	      single.type = g.type.replace('Multi','');
+	      single.coordinates = part;
+        if(g.crs) single.crs = g.crs;
+	      return single;
+	    });  
+	  } else {
+	    return false;
+	  }
+	}
+	function implode(gs) {
+	  var sameType = gs.every(function(g) { 
+	    return singles.indexOf(g.type) > -1;
+	  })
+    var crs = gs[0].crs || 0;
+    var sameCrs = gs.every(function(g) {
+      var gcrs = g.crs || 0;
+      return gcrs == crs;
+    });
+	  if(sameType && sameCrs) {
+	    var multi = {};
+	    multi.type = 'Multi' + gs[0].type;
+	    multi.coordinates = [];
+      if(crs != 0) multi.crs = crs;
+	    gs.forEach(function(g) {
+	      multi.coordinates.push(g.coordinates);
+	    });
+	    return multi;
+	  } else {
+	    return false;
+	  }
+	};
+	var multigeojson = {
+	  explode: explode,
+	  implode: implode
+	};
+	if(typeof module !== 'undefined' && module.exports) {
+	  module.exports = multigeojson;
+	} else if(window) {
+	  window.multigeojson = multigeojson;
+	}
+})();
